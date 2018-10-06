cb8eaeb467338cd039edd5218d0bb38bf2f0631f
cdeng001 <cdeng001@ucr.edu>
2018-10-06 18:20:18 -0400

    init django channels

--- gameapi/settings.py
+++ gameapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'gameapi.wsgi.application'
+ASGI_APPLICATION = "gameapi.routing.application"
 
 
 # Database
